46b19d0dc7e7653fc7c6562ad0fa2632f179734a
Juanse Gomez <juan.gomez@dowjones.com>
2024-06-05 19:37:10 -0500

    Query 2: Fixed suggestions

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,17 +1,14 @@
 CREATE OR REPLACE TABLE jsgomez14.grouping_sets_hw5 AS
 SELECT
     COALESCE(GD.player_name, 'Overall') AS player, -- Nulls means overall.
-    COALESCE(GD.team_abbreviation, 'Overall') AS team,
+    COALESCE(GD.team_abbreviation, 'Overall') AS team, -- Because of group sets leaving nulls.
     COALESCE(CAST(G.season AS VARCHAR), 'Overall') AS season,
     SUM(pts) AS total_points, -- Total points
-    AVG(pts) AS avg_points,
-    MIN(pts) AS min_points,
-    MAX(pts) AS max_points,
     SUM(
      IF(
        (team_id = home_team_id AND home_team_wins = 1)
        OR (team_id = visitor_team_id AND home_team_wins = 0)
-     , 1, 0)
+     , 1, 0) -- Total wins
      ) AS won_games -- Count won games
 FROM bootcamp.nba_game_details_dedup AS GD -- Using dedduped data.
 JOIN bootcamp.nba_games AS G
