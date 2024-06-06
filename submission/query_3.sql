1228c03eee2f5a5715b986c80044e20697149d8e
Juanse Gomez <juan.gomez@dowjones.com>
2024-06-05 19:31:37 -0500

    Query 3: Player w more points in single team.

---
+++ submission/query_3.sql
@@ -0,0 +1,5 @@
+SELECT *
+FROM jsgomez14.grouping_sets_hw5
+WHERE player != 'Overall' AND team != 'Overall' AND season = 'Overall'
+ORDER BY total_points DESC 
+-- LeBron James for Cleveland Cavaliers
