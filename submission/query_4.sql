a65b482783c43e4c8da3a9f66aac1d115342801c
mposada97 <85133902+mposada97@users.noreply.github.com>
2024-06-06 21:53:25 -0500

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -6,7 +6,11 @@ FROM
   mposada.hw_5_q_2
 WHERE 
   aggregation_level = 'player_name__season' AND
-  total_points IS NOT NULL
+  total_points IS NOT NULL AND
+  total_points = (
+    SELECT MAX(total_points) -- we select max because this will bring in more than one player if theres a tie for first place
+    FROM mposada.hw_5_q_2
+    WHERE aggregation_level = 'player_name__season'
+  )
 ORDER BY 
-  total_points DESC
-LIMIT 1 -- kevin durant scored the most points in one season
+  total_points DESC-- kevin durant scored the most points in one season
