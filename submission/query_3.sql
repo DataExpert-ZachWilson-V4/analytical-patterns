cdca4b6661123db2e24a9370229ed8df785a362a
mposada97 <85133902+mposada97@users.noreply.github.com>
2024-06-06 21:56:00 -0500

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -6,7 +6,11 @@ FROM
   mposada.hw_5_q_2
 WHERE 
   aggregation_level = 'player_name__team_name' AND
-  total_points IS NOT NULL
+  total_points IS NOT NULL AND
+  total_points = (
+    SELECT MAX(total_points) -- we select max because this will bring in more than one player if theres a tie for first place
+    FROM mposada.hw_5_q_2
+    WHERE aggregation_level = 'player_name__team_name'
+  )
 ORDER BY 
-  total_points DESC
-LIMIT 1
+  total_points DESC -- answer is LeBron James for cleveland
