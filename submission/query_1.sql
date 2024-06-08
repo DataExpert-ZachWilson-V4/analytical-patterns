bfe117b7dc12c76eae748acb2a8f82f8c1fbd036
Srikanth Sudhindra <srikanthsudhindra@gmail.com>
2024-06-07 23:06:41 -0700

    Query1 formatting

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,35 +1,35 @@
 -- Build a CTE of previous season
-with last_season as (
-    select * 
-    from bootcamp.nba_players
-    where current_season = 1999
+WITH last_season AS (
+    SELECT * 
+    FROM bootcamp.nba_players
+    WHERE current_season = 1999
 ),
 -- CTE for current season
-current_season as (
-    select * 
-    from bootcamp.nba_players
-    where current_season = 2000
+current_season AS (
+    SELECT * 
+    FROM bootcamp.nba_players
+    WHERE current_season = 2000
 ),
 -- CTE for final result
-result as (
-    select
-        coalesce(l.player_name, c.player_name) as player_name,
+result AS (
+    SELECT
+        coalesce(l.player_name, c.player_name) AS player_name,
         CASE
             -- Last season year column is empty + current season is not and seasons cummulative array is empty
-            WHEN l.current_season is null and c.current_season is not null and l.seasons is null then 'New'
+            WHEN l.current_season IS NULL and c.current_season IS NOT NULL and l.seasons IS NULL THEN 'New'
             -- Last season year is not empty + current season is not empty
-            WHEN l.current_season is not null and c.current_season is null then 'Retired'
+            WHEN l.current_season IS NOT NULL and c.current_season IS NULL THEN 'Retired'
             -- Last season year is not empty + current season is not empty
-            WHEN l.current_season is not null and c.current_season is not null then 'Continued Playing'
+            WHEN l.current_season IS NOT NULL and c.current_season IS NOT NULL THEN 'Continued Playing'
             -- Last season year is empty + current season year is not + seasons cumulative array is not empty
-            WHEN l.current_season is null and c.current_season is not null and l.seasons is not null then 'Returned from Retirement'
-            else 'Stayed Retired'
+            WHEN l.current_season IS NULL and c.current_season IS NOT NULL and l.seasons IS NOT NULL THEN 'Returned FROM Retirement'
+            ELSE 'Stayed Retired'
         END AS change_tracking,
         c.current_season
-    from last_season l
-    full outer join current_season c
-        on c.player_name = l.player_name
+    FROM last_season l
+    FULL OUTER JOIN current_season c
+        ON c.player_name = l.player_name
 )
-select 
+SELECT 
     *
-from result
+FROM result
