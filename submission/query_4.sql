f32a52b766402ee0d8d05b80da18cc2fb5c554cb
videetm <vids107@gmail.com>
2024-06-08 14:58:42 -0400

    week 5 hw query 3,4 & 5

---
+++ submission/query_4.sql
@@ -0,0 +1,16 @@
+SELECT
+    player_name,
+    team,
+    points
+FROM
+    videet.nba_grouping_sets
+    -- Only include rows where the data represents a combination of a single player and a season, and where points are available
+WHERE
+    aggregation_level = 'player_plus_season'
+    AND points IS NOT NULL
+    -- Sort the rows in descending order based on the points to bring the highest scorer to the top
+ORDER BY
+    points DESC
+    -- Return only the top scoring record
+LIMIT
+    1 -- the top player is Kevin Durant
