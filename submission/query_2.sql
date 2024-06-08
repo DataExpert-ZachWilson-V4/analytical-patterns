64cd710cbb1bdb2f5bab4d66a4d76e1e07b29a91
Billy Switzer <billy.switzer@gmail.com>
2024-06-08 17:54:45 -0400

    Switzer Analytical Patterns Homework

---
+++ submission/query_2.sql
@@ -0,0 +1,101 @@
+--Below is a query that slices the nba_game_details (joined with nba_games) on the following:
+--		player and team
+--		player and season
+--		team
+--Both the nba_games and nba_game_details tables need to be deduped, so the first four queries accomplish that goal
+--The nba_game_details_deduped.player_order column is used in the final query to provide a means to sum the wins at the team grain, rather than the team/player grain
+--The data does include games from the pre- and post-season.
+
+WITH nba_game_details_full AS (
+	SELECT team_id,
+		game_id,
+		team_abbreviation,
+		player_name,
+		fgm,
+		fga,
+		fg3m,
+		fg3a,
+		ftm,
+		fta,
+		oreb,
+		dreb,
+		reb,
+		ast,
+		stl,
+		blk,
+		to,
+		pf,
+		pts,
+		ROW_NUMBER() OVER (PARTITION BY game_id, team_id, player_id) AS row_number
+	FROM bootcamp.nba_game_details 
+), 
+nba_game_details_deduped AS (
+	SELECT team_id,
+		game_id,
+		team_abbreviation,
+		player_name,
+		fgm,
+		fga,
+		fg3m,
+		fg3a,
+		ftm,
+		fta,
+		oreb,
+		dreb,
+		reb,
+		ast,
+		stl,
+		blk,
+		to,
+		pf,
+		pts,
+		ROW_NUMBER() OVER (PARTITION BY team_id, game_id ORDER BY player_name) AS player_order
+	FROM nba_game_details_full
+	WHERE row_number = 1
+),
+nba_games_full AS (
+	SELECT game_id,
+		season,
+		team_id_home,
+		home_team_wins,
+		ROW_NUMBER() OVER (PARTITION BY game_id) AS row_number
+	FROM bootcamp.nba_games
+),
+nba_games_deduped AS (
+	SELECT game_id,
+		season,
+		team_id_home,
+		home_team_wins
+	FROM nba_games_full
+	WHERE row_number = 1
+)
+SELECT COALESCE(player_name,'(overall)') AS player_name,
+	COALESCE(team_abbreviation,'(overall)') AS team_abbreviation,
+	COALESCE(CAST(season AS VARCHAR),'(overall)') AS season,
+	SUM(fgm) AS fgm,
+	SUM(fga) AS fga,
+	CASE WHEN SUM(fga) > 0 THEN SUM(fgm) / SUM(fga) END AS fgpct,
+	SUM(fg3m) AS fg3m,
+	SUM(fg3a) AS fg3a,
+	CASE WHEN SUM(fg3a) > 0 THEN SUM(fg3m) / SUM(fg3a) END AS fg3pct,
+	SUM(ftm) AS ftm,
+	SUM(fta) AS fta,
+	CASE WHEN SUM(fta) > 0 THEN SUM(ftm) / SUM(fta) END AS ftpct,
+	SUM(oreb) AS oreb,
+	SUM(dreb) AS dreb,
+	SUM(reb) AS reb,
+	SUM(ast) AS ast,
+	SUM(stl) AS stl,
+	SUM(blk) AS blk,
+	SUM(to) AS to,
+	SUM(pf) AS pf,
+	SUM(pts) AS pts,
+	SUM(CASE WHEN ngd.team_id = ng.team_id_home THEN ng.home_team_wins ELSE 1 - ng.home_team_wins END) AS player_wins,
+	SUM(CASE WHEN ngd.player_order = 1 THEN CASE WHEN ngd.team_id = ng.team_id_home THEN ng.home_team_wins ELSE 1 - ng.home_team_wins END END) AS team_wins
+FROM nba_game_details_deduped ngd
+	JOIN nba_games_deduped ng ON ngd.game_id = ng.game_id
+GROUP BY GROUPING SETS (
+	(player_name, team_abbreviation),
+	(player_name, season),
+	(team_abbreviation)
+)
---
+++ submission/query_7.sql
@@ -0,0 +1,35 @@
+--Again, the nba_game_details and nba_games tables need to be deduped. Since we only want points scored for each game by LeBron James,
+--it makes sense to join these tables at the beginning and dedupe the result set.
+--This query utilizes the same streak logic which can be used to track state changes for a full load of a Type 2 SCD.
+--Here the streak_identifier identifies streaks where LeBron either scored above ten points in each game, or scored ten points or fewer in each game.
+--The highest number of games in a particular streak where scored_over_ten_pts = 1 identifies the number of games in a row in which LeBron scored over ten points.
+WITH lebron_nba_game_details_deduped AS (
+	SELECT DISTINCT ng.game_date_est,
+		ngd.pts		
+	FROM bootcamp.nba_game_details ngd
+		JOIN bootcamp.nba_games ng ON ngd.game_id = ng.game_id
+	WHERE player_name = 'LeBron James'
+),
+game_results AS (
+	SELECT game_date_est,
+		CASE WHEN pts > 10 THEN 1 ELSE 0 END AS scored_over_ten_pts
+	FROM lebron_nba_game_details_deduped
+), 
+lagged AS (
+	SELECT game_date_est,
+		scored_over_ten_pts,
+		LAG(scored_over_ten_pts, 1, 0) OVER (ORDER BY game_date_est) AS scored_over_ten_pts_lagged
+	FROM game_results
+),
+streaks AS (
+	SELECT game_date_est,
+		scored_over_ten_pts,
+		SUM(CASE WHEN scored_over_ten_pts <> scored_over_ten_pts_lagged THEN 1 ELSE 0 END) OVER (ORDER BY game_date_est) AS streak_identifier
+	FROM lagged 
+)
+SELECT COUNT(1) AS number_of_games
+FROM streaks
+WHERE scored_over_ten_pts = 1		--Capture streaks where LeBron scored over ten points, not where he scored ten points or less
+GROUP BY streak_identifier
+ORDER BY COUNT(1) DESC
+LIMIT 1
