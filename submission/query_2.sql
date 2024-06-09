f5f97c6c35b3d104b0ed7e346a99a83aae66cb23
Youkang Shon <ykshon527@gmail.com>
2024-06-09 11:05:49 -0500

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,14 +1,15 @@
+CREATE OR REPLACE TABLE ykshon52797255.nba_grouping_sets AS (
 WITH
  combined AS (
   select g.home_team_id,
     g.visitor_team_id,
-    gd.player_id,
-    gd.player_name,
+    COALESCE(gd.player_id, 0) AS player_id,
+    COALESCE(gd.player_name, 'N/A') AS player_name,
     gd.pts,
-    gd.team_id,
-    gd.team_abbreviation,
+    COALESCE(gd.team_id, 0) AS team_id,
+    COALESCE(gd.team_abbreviation, 'N/A') AS team_abbreviation,
     g.home_team_wins,
-    g.season
+    COALESCE(g.season, 9999) AS season
   from bootcamp.nba_games g
   JOIN bootcamp.nba_game_details_dedup gd
   on g.game_id = gd.game_id
@@ -16,7 +17,7 @@ WITH
   
 SELECT COALESCE(player_name, '(overall)') AS player_name,
   COALESCE(team_abbreviation, '(overall)') AS team_abbreviation,
-  COALESCE(CAST(season AS VARCHAR), '(overall)') AS season,
+  COALESCE(season, 9999) AS season,
   SUM(pts) as pts,
   CAST(COUNT(case
     when team_id = home_team_id and home_team_wins = 1 THEN 1
@@ -29,4 +30,5 @@ GROUP BY
  (player_name, team_id, team_abbreviation),
  (player_name, season),
  (team_id, team_abbreviation)
+ )
  )
