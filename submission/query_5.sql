6ef44b29c0de21c682f5c86747c3478acd2179b8
videetm <vids107@gmail.com>
2024-06-08 20:06:00 -0400

    week 5 hw query 5 fix

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -9,7 +9,7 @@ WHERE
     AND points IS NOT NULL
     -- Sort the rows in descending order based on the points to bring the highest scorer to the top
 ORDER BY
-    points DESC
+    wins DESC
     -- Return only the top scoring record
 LIMIT
     1 -- the top team is Golden State Warriors
