08757a8ad6035226e0edc322be71073fffadc222
DeeptiVarghese <31417684+DeeptiVarghese@users.noreply.github.com>
2024-06-10 15:13:04 -0700

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -1,6 +1,6 @@
 with player_season as(
 select player_name,season,sum(points) as points
-from deeptianievarghese22866.game_details_grouping
+from game_details_grouping
 group by 1,2
 )
 select player_name
