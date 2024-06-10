70c9292e5c65cfa596cdf469df60a7c305061957
DeeptiVarghese <31417684+DeeptiVarghese@users.noreply.github.com>
2024-06-10 15:39:56 -0700

    Update query_2.sql

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,19 +1,28 @@
-create or replace table game_details_grouping as
-with combined as(
-select *
-from bootcamp.nba_game_details d
-inner join
-bootcamp.nba_games g
-on d.game_id=g.game_id
-and d.team_id=g.home_team_id
+CREATE OR REPLACE TABLE game_details_grouping AS
+WITH game_details_combined AS (
+    SELECT *, ROW_NUMBER() OVER(PARTITION BY d.game_id, d.team_id, player_id ORDER BY g.game_date_est) AS row_num
+    FROM bootcamp.nba_game_details d
+    INNER JOIN bootcamp.nba_games g
+    ON d.game_id = g.game_id
+    AND d.team_id = g.home_team_id
+   
 )
 
-
-select player_name, team_id, season, 
-sum(pts) as points, sum(home_team_wins) as game_wins
-from combined
-group by grouping sets(
-(player_name,team_id),
-(player_name,season),
-(team_id)
-)
+SELECT
+    CASE 
+        WHEN GROUPING(player_name, team_abbreviation) = 0 THEN 'player_name__team_name'
+        WHEN GROUPING(player_name, season) = 0 THEN 'player_name__season'
+        WHEN GROUPING(team_abbreviation) = 0 THEN 'team_name'
+    END as aggregation_level,
+    player_name, 
+    team_abbreviation, 
+    season, 
+    SUM(pts) AS points, 
+    SUM(home_team_wins) AS game_wins
+FROM game_details_combined
+-- Filter out duplicates
+WHERE row_num = 1
+GROUP BY GROUPING SETS (
+    (player_name, team_abbreviation),
+    (player_name, season),
+    (team_abbreviation)
