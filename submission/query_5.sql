1b8f9dfb798cebe13f38335044ceaec2e4d57230
DeeptiVarghese <31417684+DeeptiVarghese@users.noreply.github.com>
2024-06-10 15:50:45 -0700

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,9 +1,9 @@
 with team_games as(
-select team_id,sum(game_wins) as game_wins
+select team_abbreviation,sum(game_wins) as game_wins
 from game_details_grouping
 group by 1
 )
-select team_id
+select team_abbreviation
 from team_games
 where team_id is not null
 order by game_wins desc
