075054bbccde65ad4ba0de0fd075fadafbcbbc1c
mandrulliBF <martina.andrulli@burda-forward.it>
2024-06-10 10:32:30 +0200

    Update Query 3 and 4

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,9 +1,8 @@
 -- Which player scored the most points playing for a single team?
-SET @aggregation_level = 'player_team';
 
 SELECT player, team, total_points
 FROM game_details_dashboard
-WHERE aggregation_level = @aggregation_level 
+WHERE aggregation_level = 'player_team' 
 AND total_points IS NOT NULL
 ORDER BY total_points DESC 
 LIMIT 1
--- submission/query_4.sql
+++ submission/query_4.sql
@@ -1,9 +1,8 @@
 -- Which player scored the most points in one season?
-SET @aggregation_level = 'player_season';
 
 SELECT player, team, total_points
 FROM game_details_dashboard
-WHERE aggregation_level = @aggregation_level 
+WHERE aggregation_level = 'player_season'
 AND total_points IS NOT NULL
 ORDER BY total_points DESC 
 LIMIT 1
