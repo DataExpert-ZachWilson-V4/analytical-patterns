8272fc29ee82fa5ebdf317e01203bd82c95d5dee
DeeptiVarghese <31417684+DeeptiVarghese@users.noreply.github.com>
2024-06-10 17:19:38 -0700

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -3,9 +3,7 @@ select team_abbreviation,sum(game_wins) as game_wins
 from game_details_grouping
 where aggregation_level = 'player_name__team_name'
 group by 1
-),
-query_5 as(
+)
 select team_abbreviation
 from team_wins_summary
 where game_wins=(select max(game_wins) from team_wins_summary)
-)
