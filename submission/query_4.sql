e3100309ce8f02cb8e738297fa9cd876788368a3
DeeptiVarghese <31417684+DeeptiVarghese@users.noreply.github.com>
2024-06-10 17:04:43 -0700

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -1,7 +1,7 @@
 with player_season as(
 select player_name,season,sum(points) as points
 from game_details_grouping
-where aggregation_level='player_name__season’
+where aggregation_level='player_name__season'
 group by 1,2
 )
 select player_name
