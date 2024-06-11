a5df5493f03a42d86b2f2fe3e83cbf6b7693ff55
DeeptiVarghese <31417684+DeeptiVarghese@users.noreply.github.com>
2024-06-11 12:01:44 -0700

    Update query_6.sql

--- submission/query_6.sql
+++ submission/query_6.sql
@@ -2,11 +2,11 @@ with home_team_count as(
 SELECT home_team_id, sum(home_team_wins) over(partition by home_team_id order by game_id rows between 45 preceding and 45 following) as count_wins
 FROM bootcamp.nba_games
 ),
-wins_by_team as
+wins_by_team as(
 select home_team_id, max(count_wins) as max_wins_by_team
 from home_team_count
 group by 1
-
+)
 
 select distinct team_abbreviation,max_wins_by_team
 from wins_by_team w
