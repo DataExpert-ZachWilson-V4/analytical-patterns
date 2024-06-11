9f2cf73bb4a0bef3bac75c7d830585906ff1dfe3
DeeptiVarghese <31417684+DeeptiVarghese@users.noreply.github.com>
2024-06-10 17:07:32 -0700

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,6 +1,6 @@
 with team_wins_summary as(
 select team_abbreviation,sum(game_wins) as game_wins
-from deeptianievarghese22866.game_details_grouping
+from game_details_grouping
 where aggregation_level = 'player_name__team_name'
 group by 1
 )
