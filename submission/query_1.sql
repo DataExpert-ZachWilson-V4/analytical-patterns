bcca2ec9996dc0c0c4bf15a4e5d1752ba84d5b29
Ameena Ansari <ameena5432@gmail.com>
2024-06-12 10:52:47 -0700

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,14 +1,3 @@
-'''
- Write a query (`query_1`) that does state change tracking for `nba_players`.
- Create a state change-tracking field that takes on the following values:
-  - A player entering the league should be `New`
-  - A player leaving the league should be `Retired`
-  - A player staying in the league should be `Continued Playing`
-  - A player that comes out of retirement should be `Returned from Retirement`
-  - A player that stays out of the league should be `Stayed Retired`
-'''
-
-
 INSERT into nba_players_track_status 
 -- Define the 'yesterday' CTE to capture player data from the 2000 season
 WITH yesterday AS (
