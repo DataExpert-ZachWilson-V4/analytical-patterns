9fff03e298a9784090b810e4f45d1c3bf2597fc9
AliaHa3 <eng.alia.hamwi@gmail.com>
2024-06-12 05:34:28 +0300

    format queries as CTEs

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,26 +1,35 @@
 -- This query calculates total points scored in NBA games, providing subtotals and overall totals.
-select
+with
+    game_details_cte AS (
+        select
+            ngd.*,
+            ng.season
+        from
+            -- Join game details with games metadata
+            bootcamp.nba_game_details_dedup ngd -- using pre-deduped table that exist in bootcamp schema
+            inner join bootcamp.nba_games ng on ngd.game_id = ng.game_id
+    )
+SELECT
     -- Use COALESCE to handle nulls, defaulting to 'Overall' for aggregate rows
-    coalesce(ngd.player_name, 'Overall') as player,
-    coalesce(ngd.team_abbreviation, 'Overall') as team,
-    coalesce(cast(ng.season as varchar), 'Overall') as season,
+    coalesce(player_name, 'Overall') as player,
+    coalesce(team_abbreviation, 'Overall') as team,
+    coalesce(cast(season as varchar), 'Overall') as season,
+    
     -- Sum points, treating nulls as 0
     sum(
         case
-            when ngd.pts is null then 0
-            else ngd.pts
+            when pts is null then 0
+            else pts
         end
     ) as total_points
 from
-    -- Join game details with games metadata
-    bootcamp.nba_game_details_dedup ngd -- using pre-deduped table that exist in bootcamp schema
-    inner join bootcamp.nba_games ng on ngd.game_id = ng.game_id
+    game_details_cte
 group by
     -- Grouping sets to create subtotals for each player, team, and season
     GROUPING SETS (
-        (ngd.player_name, ngd.team_abbreviation),
-        (ngd.player_name, ng.season),
-        (ngd.team_abbreviation)
+        (player_name, team_abbreviation),
+        (player_name, season),
+        (team_abbreviation)
     )
 order by
     -- Order results by player, team, and season
