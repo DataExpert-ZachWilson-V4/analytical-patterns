e5b440bcecac7761559bbef3eb07e9c9202d3b39
Sagar Arora <sagararora@Sagars-MBP.hsd1.il.comcast.net>
2024-06-12 00:03:33 -0500

    fixing errors

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,6 +1,5 @@
 -- This query gets the dataset which team has won the most games
 
-CREATE OR REPLACE VIEW query_5 AS
 SELECT team, won_games
 FROM sagararora492.grouping_sets
 WHERE aggregation_level = 'team'
