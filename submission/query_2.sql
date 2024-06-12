6fcfbd45f5a53839e0e278ae48dad13689f8a78e
Jesse Charbneau <jessecharbneau@gmail.com>
2024-06-11 20:16:41 -0400

    simplified the code a bit, added more comments, added create statement, as well as insert

--- submission/query_2.sql
+++ submission/query_2.sql
@@ -1,25 +1,31 @@
+-- Write a query (query_2) that uses GROUPING SETS to perform aggregations of the nba_game_details data.
+-- Create slices that aggregate along the following combinations of dimensions:
+--
+-- player and team
+-- player and season
+-- team
+--
+-- CREATE TABLE jlcharbneau.nba_grouping_sets (
+--     aggregation_level   VARCHAR,
+--     player_name         VARCHAR,
+--     team_abbreviation   VARCHAR,
+--     season              VARCHAR,
+--     total_points        DOUBLE,
+--     total_rebounds      DOUBLE,
+--     total_assists       DOUBLE,
+--     player_wins         BIGINT,
+--     team_wins           BIGINT
+-- )
+-- WITH (
+--     format = 'PARQUET',
+--     format_version = 2,
+--     location = 's3://zachwilsonsorganization-522/ce557692-2f28-41e8-8250-8608042d2acb/8ada082a-be6f-4fd3-997b-c00eb68297b4'
+-- )
+--
+insert into jlcharbneau.nba_grouping_sets
 -- Deduplicating nba_game_details table to ensure unique player entries per game
 WITH nba_game_details_full AS (
-    SELECT
-        game_id,
-        team_id,
-        team_abbreviation,
-        player_name,
-        fgm,
-        fga,
-        fg3m,
-        fg3a,
-        ftm,
-        fta,
-        oreb,
-        dreb,
-        reb,
-        ast,
-        stl,
-        blk,
-        to,
-        pf,
-        pts,
+    SELECT *,
         -- Using ROW_NUMBER to identify duplicate player entries per game
         ROW_NUMBER() OVER (PARTITION BY game_id, team_id, player_id) AS row_number
     FROM academy.bootcamp.nba_game_details
@@ -27,26 +33,7 @@ WITH nba_game_details_full AS (
 
 -- Keeping only the first occurrence of each player entry per game
 nba_game_details_deduped AS (
-    SELECT
-        game_id,
-        team_id,
-        team_abbreviation,
-        player_name,
-        fgm,
-        fga,
-        fg3m,
-        fg3a,
-        ftm,
-        fta,
-        oreb,
-        dreb,
-        reb,
-        ast,
-        stl,
-        blk,
-        to,
-        pf,
-        pts,
+    SELECT *,
         -- Using ROW_NUMBER to identify the primary player entry for ordering within each team-game combination
         ROW_NUMBER() OVER (PARTITION BY team_id, game_id ORDER BY player_name) AS player_order
     FROM nba_game_details_full
