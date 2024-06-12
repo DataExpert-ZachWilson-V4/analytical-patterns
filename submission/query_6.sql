74118fd60f7307233860f44a3ac2fe89964edf62
supaman-go <154898872+supaman-go@users.noreply.github.com>
2024-06-11 21:21:37 -0700

    Updated query 6 from feedback. Added lines to queries 1 through 5 and 7 for feedback.

--- submission/query_6.sql
+++ submission/query_6.sql
@@ -1,43 +1,69 @@
--- CTE to record game data for each date
-WITH distinct_game_data AS (
-    SELECT 
-        DISTINCT game_date_est, 
+-- CTE to record game details data for each unique player per game
+WITH nba_game_details_deduped AS (
+    SELECT DISTINCT
         game_id,
-        home_team_id,
-        visitor_team_id,
-        home_team_wins
+        team_id,
+        team_abbreviation
+    FROM bootcamp.nba_game_details
+),
+
+-- CTE to record game data for each unique game
+nba_games_deduped AS (
+    SELECT DISTINCT
+        game_id,
+        team_id_home,
+        home_team_wins,
+        game_date_est
     FROM bootcamp.nba_games
 ),
--- CTE to record data combined over deduped data for each team
-combined AS (
-    SELECT 
-        game_date_est AS game_date,
-        home_team_id AS team_id,
-        home_team_wins AS game_win
-    FROM distinct_game_data
-        
-    union all
-    
-    SELECT 
-        game_date_est AS game_date,
-        visitor_team_id AS team_id,
-    CASE 
-        WHEN home_team_wins = 0 THEN 1
-        WHEN home_team_wins = 1 THEN 0 
-    end AS game_win
-    FROM distinct_game_data
-    WHERE home_team_wins IS NOT NULL
+
+-- CTE to calculate cummulative wins
+cummulative_team_wins AS (
+    SELECT
+        gd.team_id,
+        gd.game_id,
+        gd.team_abbreviation,
+        g.game_date_est,
+        SUM(
+            CASE
+                WHEN gd.team_id = g.team_id_home THEN g.home_team_wins
+                ELSE 1 - g.home_team_wins
+            END
+        ) OVER (PARTITION BY gd.team_id ORDER BY g.game_date_est) AS cumulative_wins
+    FROM
+        nba_game_details_deduped gd
+        JOIN nba_games_deduped g ON gd.game_id = g.game_id
 ),
+
 -- CTE to record total wins OVER a 90 game sliding window
 over_90_game_sliding AS (
-    SELECT 
-        team_id, 
-        game_date - INTERVAL '90' DAY AS starting,
-        game_date AS ending, 
-        SUM(game_win) OVER (partition by team_id order by game_date ROWS BETWEEN 89 PRECEDING AND CURRENT ROW) AS total_wins
-    FROM combined
+    SELECT
+        team_id,
+        game_id,
+        team_abbreviation,
+        cumulative_wins - LAG(cumulative_wins, 90, 0) OVER (PARTITION BY team_id ORDER BY game_date_est) AS rolling_90_game_wins
+    FROM
+        cummulative_team_wins
+),
+
+-- CTE to track team with most wins
+max_rolling_wins AS (
+    SELECT
+        team_id,
+        team_abbreviation,
+        MAX(rolling_90_game_wins) AS max_wins_over_90_games
+    FROM
+        over_90_game_sliding
+    GROUP BY
+        team_id,
+        team_abbreviation
 )
-SELECT 
-    MAX_BY(team_id, total_wins) AS team_id,
-    MAX(total_wins) AS total_game_wins
-FROM over_90_game_sliding
+
+SELECT
+    team_abbreviation,
+    max_wins_over_90_games
+FROM
+    max_rolling_wins
+ORDER BY
+    max_wins_over_90_games DESC
+LIMIT 1
