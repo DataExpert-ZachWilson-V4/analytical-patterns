f7b24ecfb060775c8972f02963724a43a4ff43d4
Mohammad.YM <Mymah592@gmail.com>
2024-06-12 21:08:38 -0400

    setting up query_1.sql

---
+++ submission/query_1.sql
@@ -0,0 +1,15 @@
+/*
+
+- Write a query (`query_1`) that does state change tracking for `nba_players`. Create a state change-tracking field that takes on the following values:
+  - A player entering the league should be `New`
+  - A player leaving the league should be `Retired`
+  - A player staying in the league should be `Continued Playing`
+  - A player that comes out of retirement should be `Returned from Retirement`
+  - A player that stays out of the league should be `Stayed Retired`
+  */
+
+  With yesterday as(
+
+
+    
+  )
