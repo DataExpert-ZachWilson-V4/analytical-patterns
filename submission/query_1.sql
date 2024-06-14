3f96a0914077e937d1721ef01493605a18f16a07
Grisell Reyes Rios <grisellreyesrios@Grisells-MacBook-Pro-2.local>
2024-06-14 14:48:43 -0600

    Redoing query 1 to deploy

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,71 +1,44 @@
---Obtain data from last_season
-insert into grisreyesrios.nba_players_state_tracking
-with last_season as (
-  select *
-  from grisreyesrios.nba_players_state_tracking
-  where season = 1995
-),
+--Obtain data from previous_year
 
---Obtain data for current seadon
-current_season as (
-  select
-    player_name,
-    is_active,
-    current_season
-  from bootcamp.nba_players
-  where current_season = 1996
+WITH previous_year AS (
+    SELECT *
+    FROM grisreyesrios.nba_players_state_tracking
+    WHERE year = 1995
 ),
-
-combined as (
-  select 
-    ls.last_active_season as previous_last_active_season,
-    cs.is_active,
-    coalesce(ls.player_name, cs.player_name) as player_name,
-    coalesce(
-      ls.first_active_season, if(cs.is_active, cs.current_season, null)
-    ) as first_active_season,
-    coalesce(if(cs.is_active, cs.current_season, null), ls.last_active_season)
-      as last_active_season,
-    case
-      when
-        ls.seasons_active is null and cs.is_active
-        then array[cs.current_season]
-      when
-        ls.seasons_active is not null
-        and (not cs.is_active or cs.is_active is null)
-        then ls.seasons_active
-      when
-        ls.seasons_active is not null and cs.is_active
-        then ls.seasons_active || array[cs.current_season]
-    end as seasons_active,
-    coalesce(cs.current_season, ls.season + 1) as season
-  from last_season as ls
-  full join current_season as cs
-    on ls.player_name = cs.player_name
+current_year AS (
+    SELECT player_name,
+        MAX(current_season) AS active_year
+    FROM bootcamp.nba_players
+    WHERE is_active = true
+        AND current_season = 1996
+    GROUP BY player_name
 ),
-
-final as (
-  select
-    player_name,
-    first_active_season,
-    last_active_season,
-    seasons_active,
-    --Creating a state change-tracking field
-    case
-      when season - first_active_season = 0 then 'New'
-      when season - previous_last_active_season = 1 then 'Continued Playing'
-      when
-        is_active and season - previous_last_active_season > 1
-        then 'Returned from Retirement'
-      when
-        not is_active and season - previous_last_active_season = 1
-        then 'Retired'
-      else 'Stayed Retired'
-    end as yearly_active_state,
-    
-    season
-  from combined
+combined AS (
+    SELECT COALESCE(y.player_name, t.player_name) AS player_name,
+        COALESCE(y.first_active_year, t.active_year) AS first_active_year,
+        y.last_active_year AS last_active_year_previous_year,
+        t.active_year,
+        COALESCE(t.active_year, y.last_active_year) AS last_active_year,
+        CASE
+            WHEN y.years_active IS NULL THEN ARRAY [t.active_year]
+            WHEN t.active_year IS NULL THEN y.years_active
+            ELSE y.years_active || ARRAY [t.active_year]
+        END AS years_active,
+        1996 AS partition_year
+    FROM previous_year y
+        FULL OUTER JOIN current_year t ON y.player_name = t.player_name
 )
-
-select *
-from final
+SELECT player_name,
+    first_active_year,
+    last_active_year,
+    years_active,
+    CASE
+        WHEN (active_year - first_active_year) = 0 THEN 'New' -- new player, this year is player's first year ever
+        WHEN (active_year - last_active_year_previous_year) = 1 THEN 'Continued Playing' -- player stays in league, last active year from previous year is 1 less than current year
+        WHEN (active_year - last_active_year_previous_year) > 1 THEN 'Returned from Retirement' -- player has returned, last active year from previous year is at least 1 more than current year (Michael Jordan returning in 2001 after retiring in 1997)
+        WHEN active_year IS NULL -- no player data for current year
+        AND (partition_year - last_active_year) = 1 THEN 'Retired' -- and current year is 1 year after last active year
+        ELSE 'Stayed Retired'
+    END AS active_state,
+    partition_year
+FROM combined
