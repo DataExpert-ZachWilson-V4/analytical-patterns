66f6b90b308ebd49c4fd1aebf4421e2c3dd4352c
codetutelage <69841209+codetutelage@users.noreply.github.com>
2024-06-14 14:55:53 -0700

    Update query_6.sql

--- submission/query_6.sql
+++ submission/query_6.sql
@@ -1,10 +1,6 @@
 /*
-
 - Write a query (`query_6`) that uses window functions on `nba_game_details` to answer the question: "What is the most games a single team has won in a given 90-game stretch?"
-
-
 */
-
 
 WITH game_deduped AS (
   SELECT
