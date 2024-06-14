55c46a2ad90fe266526cc9e458a24c93748fd730
Evan Brunt <47375706+evan-brunt@users.noreply.github.com>
2024-06-13 21:16:13 -0400

    Update query_3.sql - improved query by using aggregation level

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,6 +1,16 @@
-WITH individual_team_scoring AS (
-  SELECT player, team, total_points, dense_rank() over (order by total_points desc) as rank
-  FROM ebrunt.game_details_dashboard
-  WHERE player <> 'overall' AND team <> 'overall' AND total_points IS NOT NULL
-)
-SELECT * FROM individual_team_scoring WHERE rank = 1
+SELECT
+  player,
+  team,
+  total_points
+FROM
+  ebrunt.game_details_dashboard
+WHERE
+  aggregation_level = 'player_team'
+  AND total_points = (
+    SELECT
+      MAX(total_points)
+    FROM
+      ebrunt.game_details_dashboard
+    WHERE
+      aggregation_level = 'player_team'
+  )
