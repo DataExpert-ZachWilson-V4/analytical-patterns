25f56c0596834190c79c93a6483de4a0edea07ed
codetutelage <69841209+codetutelage@users.noreply.github.com>
2024-06-14 14:59:17 -0700

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,6 +1,5 @@
 /*
-
-- Write a query (`query_1`) that does state change tracking for `nba_players`. Create a state change-tracking field that takes on the following values:
+Write a query (`query_1`) that does state change tracking for `nba_players`. Create a state change-tracking field that takes on the following values:
   - A player entering the league should be `New`
   - A player leaving the league should be `Retired`
   - A player staying in the league should be `Continued Playing`
