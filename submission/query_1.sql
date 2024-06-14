ff42957f1d06d7c8f327ab05cbda2e3a2c65aec5
Grisell Reyes Rios <grisellreyesrios@Grisells-MacBook-Pro-2.local>
2024-06-14 17:09:24 -0600

    Adding change complete on query 1 to pass check

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,48 +1,55 @@
---Obtain data from previous_year 
-create or replace table grisreyesrios.nba_game_details_aggregations as (
-
-WITH previous_year AS (
-    SELECT *
-    FROM grisreyesrios.nba_players_state_tracking
-    WHERE year = 1995
-),
---current year data
-current_year AS (
-    SELECT player_name,
-        MAX(current_season) AS active_year
-    FROM bootcamp.nba_players
-    WHERE is_active = true
-        AND current_season = 1996
-    GROUP BY player_name
-),
---combined
-combined AS (
-    SELECT COALESCE(y.player_name, t.player_name) AS player_name,
-        COALESCE(y.first_active_year, t.active_year) AS first_active_year,
-        y.last_active_year AS last_active_year_previous_year,
-        t.active_year,
-        COALESCE(t.active_year, y.last_active_year) AS last_active_year,
-        CASE
-            WHEN y.years_active IS NULL THEN ARRAY [t.active_year]
-            WHEN t.active_year IS NULL THEN y.years_active
-            ELSE y.years_active || ARRAY [t.active_year]
-        END AS years_active,
-        1996 AS partition_year
-    FROM previous_year y
-        FULL OUTER JOIN current_year t ON y.player_name = t.player_name
-)
-SELECT player_name,
-    first_active_year,
-    last_active_year,
-    years_active,
+--Insert into state tracking
+INSERT INTO grisreyesrios.nba_players_state_tracking
+WITH
+    yesterday AS (
+        SELECT
+            *
+        FROM
+            grisreyesrios.nba_players_state_tracking
+        WHERE
+            season = 1995 --parametrization
+    ),
+    today AS (
+        SELECT
+            player_name,
+            current_season
+        FROM
+            bootcamp.nba_players
+        WHERE
+            current_season = 1996 --parametrization
+            and is_active = true
+    ),
+    combined as (
+        SELECT
+            COALESCE(y.player_name, t.player_name) as player_name,
+            COALESCE(y.first_active_season, t.current_season) as first_active_season,
+            COALESCE(t.current_season, y.last_active_season) as last_active_season,
+            CASE
+                WHEN y.seasons_active IS NULL THEN ARRAY[t.current_season]
+                WHEN t.current_season IS NULL THEN y.seasons_active
+                ELSE y.seasons_active || ARRAY[t.current_season]
+            END as seasons_active,
+            y.last_active_season as active_previous_season,
+            t.current_season as active_season,
+            1996 as season -- parametrization
+        FROM
+            yesterday y
+            FULL OUTER JOIN today t ON y.player_name = t.player_name
+    )
+--final table
+SELECT
+    player_name,
+    first_active_season,
+    last_active_season,
+    seasons_active,
     CASE
-        WHEN (active_year - first_active_year) = 0 THEN 'New' -- new player, this year is player's first year ever
-        WHEN (active_year - last_active_year_previous_year) = 1 THEN 'Continued Playing' -- player stays in league, last active year from previous year is 1 less than current year
-        WHEN (active_year - last_active_year_previous_year) > 1 THEN 'Returned from Retirement' -- player has returned, last active year from previous year is at least 1 more than current year (Michael Jordan returning in 2001 after retiring in 1997)
-        WHEN active_year IS NULL -- no player data for current year
-        AND (partition_year - last_active_year) = 1 THEN 'Retired' -- and current year is 1 year after last active year
+        WHEN active_season - first_active_season = 0 THEN 'New'
+        WHEN active_season - active_previous_season > 1 THEN 'Returned from Retirement'
+        WHEN active_season - last_active_season = 0 THEN 'Continued Playing'
+        WHEN active_season IS NULL AND season - last_active_season = 1 THEN 'Retired'
         ELSE 'Stayed Retired'
-    END AS active_state,
-    partition_year
-FROM combined
-)
+    END as season_active_state,
+    season
+FROM
+    combined
+
