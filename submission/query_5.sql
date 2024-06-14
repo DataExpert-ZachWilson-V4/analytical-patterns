5ffe95155f6b293f9dbc6f2b5898385e6de6fa62
Evan Brunt <47375706+evan-brunt@users.noreply.github.com>
2024-06-13 21:26:03 -0400

    Update query_5.sql

---
+++ submission/query_5.sql
@@ -0,0 +1,15 @@
+SELECT
+  team,
+  total_wins
+FROM
+  ebrunt.game_details_dashboard
+WHERE
+  aggregation_level = 'team'
+  AND total_wins = (
+    SELECT
+      MAX(total_wins)
+    FROM
+      ebrunt.game_details_dashboard
+    WHERE
+      aggregation_level = 'team'
+  )
