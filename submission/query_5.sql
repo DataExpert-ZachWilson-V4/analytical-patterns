a9f680e31f3ac8c09ae5fda2d82f92e3331787de
Shabab <22752622+shababmoali@users.noreply.github.com>
2024-06-14 17:46:15 -0700

    working q3 q4 q5

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,2 +1,13 @@
-- Build additional queries on top of the results of the `GROUPING SETS` aggregations above to answer the following questions:
-  - Write a query (`query_5`) to answer: "Which team has won the most games"
+-- - Build additional queries on top of the results of the `GROUPING SETS` aggregations above to answer the following questions:
+--   - Write a query (`query_5`) to answer: "Which team has won the most games"
+
+-- team_abbreviation	max_wins
+-- SAS	                1182
+--
+select
+    team_abbreviation,
+    MAX(team_wins) AS max_wins
+from shabab.nba_grouping_sets where aggregation_level = 'team'
+group by team_abbreviation
+order by max_wins desc
+limit 1
