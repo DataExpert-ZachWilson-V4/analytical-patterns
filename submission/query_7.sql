2ab52521a52201254cb120de6d7c4e7f901bc06e
Shabab <22752622+shababmoali@users.noreply.github.com>
2024-06-14 18:11:21 -0700

    remove queries for gha

--- submission/query_7.sql
+++
@@ -1,4 +0,0 @@
--- - Write a query (`query_7`) that uses window functions on `nba_game_details` to answer the question: "How many games in a row did LeBron James score over 10 points a game?"
-
--- for gha
-select * from from shabab.nba_grouping_sets
