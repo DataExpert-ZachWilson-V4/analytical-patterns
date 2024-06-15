0ee92d1b21f03621a3072af0b1d34082e3cabc81
hdamerla <harshithadamerla@gmail.com>
2024-06-14 20:15:37 -0500

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,10 +1,15 @@
+---- Create a CTE that ranks players by total points scored for a team
+
 WITH player_team_maxpoints AS
 (
 SELECT 
  *,
+  -- Rank players by total points in descending order
   Dense_Rank() OVER(ORDER BY total_points DESC) AS rnk 
 FROM hdamerla.nba_grouping_sets WHERE aggregation_level = 'Player_Team'
+  AND total_points IS NOT NULL
 )
+-- Select the player with the highest rank (i.e., the most points)
 SELECT 
   player,
   Team,
