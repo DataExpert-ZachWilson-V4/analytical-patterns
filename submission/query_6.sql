6a13054b8fa18e8268ecf6058f60945048016354
Shabab <22752622+shababmoali@users.noreply.github.com>
2024-06-14 17:58:00 -0700

    dummy queries for gha

--- submission/query_6.sql
+++ submission/query_6.sql
@@ -1,1 +1,3 @@
 -- - Write a query (`query_6`) that uses window functions on `nba_game_details` to answer the question: "What is the most games a single team has won in a given 90-game stretch?"
+-- for gha
+select * from from shabab.nba_grouping_sets
--- submission/query_7.sql
+++ submission/query_7.sql
@@ -1,1 +1,4 @@
 -- - Write a query (`query_7`) that uses window functions on `nba_game_details` to answer the question: "How many games in a row did LeBron James score over 10 points a game?"
+
+-- for gha
+select * from from shabab.nba_grouping_sets
