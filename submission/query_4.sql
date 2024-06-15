6373dc419ec5ff0073322ec1d6177fb22fb10cc7
hdamerla <harshithadamerla@gmail.com>
2024-06-14 20:15:58 -0500

    Update query_4.sql

--- submission/query_4.sql
+++ submission/query_4.sql
@@ -1,12 +1,16 @@
+-- Create a temporary table of players, seasons, and total points, ranked by total points
 WITH player_season_maxpoints AS
 (
 SELECT 
   *,
   Dense_Rank() OVER(ORDER BY total_points DESC) AS ranking
-FROM hdamerla.nba_grouping_sets WHERE aggregation_level = 'Player_Season'
+FROM hdamerla.nba_grouping_sets 
+WHERE aggregation_level = 'Player_Season' AND total_points IS NOT NULL
 )
+-- Select the player and season with the highest total points
 SELECT 
-  player,
+  player_name,
   season,
-  total_points
-FROM player_season_maxpoints WHERE ranking = 1
+  total_player_points
+FROM player_season_maxpoints 
+WHERE ranking = 1
