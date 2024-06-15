96ba523f72553f294ab50555999c837b8301b180
hdamerla <harshithadamerla@gmail.com>
2024-06-14 23:11:49 -0500

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -3,12 +3,14 @@
 
 WITH teams_ranked_by_wins AS
 (
+---- Use DENSE_RANK() to rank teams by their number of wins
 SELECT 
   team,
   team_wins,
   DENSE_RANK() OVER(ORDER BY team_wins DESC) AS rnk 
 FROM hdamerla.nba_grouping_sets WHERE 
-aggregation_level = 'Team' and team_wins is not null
+aggregation_level = 'Team'  AND team_wins IS NOT NULL
+    AND team IS NOT NULL
 )
 
 ---- Modify the final SELECT to return all teams with the highest number of wins
