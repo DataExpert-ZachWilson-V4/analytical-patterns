606996b91b8aedf9c49ba830b5a2dff5397cc8ec
Amaliah Nation <amaliah.nation@gmail.com>
2024-06-22 16:31:27 -0500

    Fix total points

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,12 +1,12 @@
 --Query that defines teams that won the most game
 SELECT
     team_abbreviation,
-    total_points
+    total_games_won
 FROM
     amaliah21315.nba_game_details_grouped
 WHERE
     grouping_category = 'team_only' -- selects category by teams only
-    AND total_points IS NOT NULL --excludes unnecessary records where there are no points
+    AND total_games_won IS NOT NULL --excludes unnecessary records where there are no points
 ORDER BY
-    total_points DESC
+    total_games_won DESC
 LIMIT 1 -- select the top team
