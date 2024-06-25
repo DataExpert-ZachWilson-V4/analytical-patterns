0f1d7774b059662c9107d7eb970daf489528ca8c
Bhakti <bhakti@Bhaktis-MacBook-Pro.local>
2024-06-25 07:00:28 -0400

    analytical pattern

---
+++ submission/query_5.sql
@@ -0,0 +1,18 @@
+- Build additional queries on top of the results of the `GROUPING SETS` aggregations above to answer the following questions:
+  - Write a query (`query_3`) to answer: "Which player scored the most points playing for a single team?"
+  - Write a query (`query_4`) to answer: "Which player scored the most points in one season?"
+  - Write a query (`query_5`) to answer: "Which team has won the most games"
+*/
+
+-- query from the dashboard dataset created in Query 2
+SELECT team, won_games
+FROM bhakti.nba_games_details_board
+WHERE aggregation_level = 'team'
+ORDER BY won_games DESC
+LIMIT 1
+
+/*
+Result : SAS (Spurs)
+team	won_games
+SAS	    14881
+*/
